ba24906e1ed37434e3fa5f1d89d26a0ffd60207f
rafaeling <rafaelingugr@gmail.com>
2016-01-28 19:31:52 +0100

    Añadida conexion base de datos
    Se muestra el nombre del animal

---
+++ content/base_datos/conexion_bd.php
@@ -0,0 +1,12 @@
+<?php 
+
+	
+
+    $servername = "localhost";
+    $username = "shelter";
+    $password = "shelter";
+    $dbname = "shelter";
+    
+     
+    $conexion_bd = mysqli_connect($servername, $username, $password, $dbname);
+?>
