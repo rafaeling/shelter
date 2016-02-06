1105fe882e6755b0a25fd404bf89fee25462f3ec
erik <erik@pc>
2016-02-06 19:53:13 +0100

    nueva casa de acogida

--- content/casas_de_acogida.php
+++ content/casas_de_acogida.php
@@ -1,14 +1,14 @@
 <ul>
 	<li>
-		<a href="index.php?t=nueva_casa_de_acogida">Nueva casa_de_acogida</a>
+		<a href="index.php?t=nueva_casa_de_acogida">Nueva casa de acogida</a>
 	</li>
 		
 	<li>
-		<a href="index.php?t=modificar_casa_de_acogida">Modificar casa_de_acogida</a>
+		<a href="index.php?t=modificar_casa_de_acogida">Modificar casa de acogida</a>
 	</li>
 
 	<li>
-		<a href="index.php?t=dar_baja_casa_de_acogida">Dar de baja casa_de_acogida</a>
+		<a href="index.php?t=dar_baja_casa_de_acogida">Dar de baja casa de acogida</a>
 	</li>
 	
 				
