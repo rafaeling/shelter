1cb327fa9e6fd170209c705c363a9328cfbbd1d6
erik <erik@pc>
2016-02-06 20:18:49 +0100

    modificar / eliminar casa

--- content/casa_de_acogida/dar_baja_casa_de_acogida.php
+++ content/casa_de_acogida/dar_baja_casa_de_acogida.php
@@ -2,103 +2,76 @@
 						
 	<tbody>
 		<tr>
-			<td><h2>Dar Baja Usuario</h2></td>
+			<td><h2>Casas de acogida:</h2></td>
 		</tr>
-			<tr>
-				<td><label for="name_animal">Nombre:</label></td>
-				<td><input id="name_animal" name="name_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
 
-			<tr>
-				<td><label for="num_chip_animal">Número Chip:</label></td>
-				<td><input id="num_chip_animal" name="num_chip_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
+		<?php
 
-			<tr>
-				<td><label for="foto_animal">Foto:</label></td>
-				<td><input id="foto_animal" name="foto_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
+			include('content/base_datos/conexion_bd.php');
+
+			$sql = "SELECT * FROM casa_de_acogida";
 			
-			<tr>
-				<td><label for="especie_animal">Especie:</label></td>
-				<td><input id="especie_animal" name="especie_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
+			$res = mysqli_query($conexion_bd, $sql);
 
-			<tr>
-				<td><label for="raza_animal">Raza:</label></td>
-				<td><input id="raza_animal" name="raza_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
+			while($row = mysqli_fetch_assoc($res))
+			{
+				echo '<tr>
+						
 
-			<tr>
-				<td><label for="sexo_animal">Sexo:</label></td>
-				<td><input id="sexo_animal" name="sexo_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
+						<td>
+							<div id="usuario">
 
-			<tr>
-				<td><label for="edad_animal">Edad:</label></td>
-				<td><input id="edad_animal" name="edad_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
+								<label for="name_usuario">Nombre: </label>
+								<label id="nombre_usuario">'.$row["nombre"].'<br></label>
+								<label for="dni_usuario">DNI: </label>
+								<label id="dni_user">'.$row["DNI"].'<br></label>
+                                                                <label for="direccion">Direccion: </label>
+								<label id="direccion">'.$row["direccion"].'<br></label>
+							</div>
+						</td>
 
-			<tr>
-				<td><label for="color_animal">Color:</label></td>
-				<td><input id="color_animal" name="color_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
-			
-			<tr>
-				<td><label for="peso_animal">Peso:</label></td>
-				<td><input id="peso_animal" name="peso_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
+						<td>
+							<div id="animal">
+								<br><a href="index.php?t=userchange&user='.$row["dni_user"].'"> Eliminar </a>'.$row["nombre_user"].'</br>
+							</div>
+						</td>
+					</tr>';
+			}
 
-			<tr>
-				<td><label for="salud_animal">Salud:</label></td>
-				<td><input id="salud_animal" name="salud_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
-
-			<tr>
-				<td><label for="estado_animal">Estado:</label></td>
-				<td><input id="estado_animal" name="estado_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
-
-
-			<tr>
-				<td><label for="reservado_animal">Reservado:</label></td>
-				<td><input id="reservado_animal" name="reservado_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
-
-			<tr>
-				<td><label for="medicamentos_animal">Medicamentos:</label></td>
-				<td><input id="medicamentos_animal" name="medicamentos_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
-
-			<tr>
-				<td><label for="notas_animal">Notas:</label></td>
-				<td><input id="notas_animal" name="notas_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
-
-
-			<tr>
-				<td><label for="fecha_llegada_animal">Fecha llegada:</label></td>
-				<td><input id="fecha_llegada_animal" name="fecha_llegada_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
-
-			<tr>
-				<td><label for="ususario_registro_animal">Usuario Registros:</label></td>
-				<td><input id="usuario_registro_animal" name="usuario_registro_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
+			mysqli_close($conexion_bd);
 					
-			<tr>
-				<td><label for="direccion_encontrado_animal">Dirección Encontrado:</label></td>
-				<td><input id="direccion_encontrado_animal" name="direccion_encontrado_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
-			
-			<tr>
-				<td><label for="multimedia_animal">Multimedia:</label></td>
-				<td><input id="multimedia_animal" name="multimedia_animal" size="30" maxlength="40" type="text"></td>							
-			</tr>
-					
-					
+	
+		?>
+
 	</tbody>
 
 	
 
 </table>
+
+
+
+
+<div>
+
+	<?php
+
+/*
+		include('content/base_datos/conexion_bd.php');
+
+		$sql = "SELECT nombre_animal FROM animal WHERE chip_animal='26820455V'";
+		
+		$res = mysqli_query($conexion_bd, $sql);
+
+		$row = mysqli_fetch_assoc($res);
+
+		mysqli_close($conexion_bd);
+				
+		echo '<div> <h1>Bienvenido, '.$row['nombre_animal'].'</h1></div>';
+*/
+	?>
+
+
+
+</div>
+
