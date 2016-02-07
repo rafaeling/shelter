ae1f34a77cd0aa019f1309d33ec8a8a2edb8bde0
rafaeling <rafaelingugr@gmail.com>
2016-02-07 04:46:03 +0100

    Listar

--- content/eventos/listar_eventos.php
+++ content/eventos/listar_eventos.php
@@ -1,17 +1,18 @@
 <?php
 
-	
+    $fecha = isset($_GET["fecha"]) ? $_GET["fecha"] : '';
+
 			
     include('conexion_bd.php');
 
-    $sql = "SELECT * from evento";
+    $sql = "SELECT * FROM evento WHERE fecha='".$fecha."'";
 
     $res = mysqli_query($conexion_bd, $sql);
 
     $row = mysqli_fetch_assoc($res);
-    $row = mysqli_fetch_assoc($res);
     
-    $return = $row['notas'];
+    $return = $row['lista_correos'];
+
     
     echo $return;
         
