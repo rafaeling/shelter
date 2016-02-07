890444b42971a47de96956e24e365fbbe06540fa
AGM-GR <alejandroguerrero.gr@gmail.com>
2016-02-07 04:12:57 +0100

    asdfs

--- content/eventos/gestionar_eventos.php
+++ content/eventos/gestionar_eventos.php
@@ -234,13 +234,13 @@ frmCalendarSample.tbSelYear.options[i].selected = true;
 </select>
   
 <select name="tbSelYear" onchange='fUpdateCal(frmCalendarSample.tbSelYear.value, frmCalendarSample.tbSelMonth.value)'>
-<option value="1998">1998</option>
-<option value="1999">1999</option>
-<option value="2000">2000</option>
-<option value="2001">2001</option>
-<option value="2002">2002</option>
-<option value="2003">2003</option>
-<option value="2004">2004</option>
+<option value="2015">2015</option>
+<option value="2016">2016</option>
+<option value="2017">2017</option>
+<option value="2018">2018</option>
+<option value="2019">2019</option>
+<option value="2020">2020</option>
+<option value="2021">2021</option>
 </select>
 </td>
 </tr>
